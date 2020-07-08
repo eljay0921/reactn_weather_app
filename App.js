c43de8595fff3d4e3e43c668e6dcc31dfca50be5
jin <lj7812@gmail.com>
2020-07-08 22:17:18 +0900

    # 2.0 Start - Get location info

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
-// import { StatusBar } from "expo-status-bar";
 import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading></Loading>;
+// 이름 안써줘도 동작함.
+export default class extends React.Component {
+  getLocation = async () => {
+    let loc = await Location.getCurrentPositionAsync();
+    console.log(loc);
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }
